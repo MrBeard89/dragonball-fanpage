74f4126048da5506fb7d500b42faebb06637585c
Norbert Kovács <k.n.89@outlook.hu>
2023-01-16 11:40:29 +0100

    imported comics images

--- src/components/Comics.jsx
+++ src/components/Comics.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import '../styles/Comics.css'
 
+//Comics images
+import Dbz from '../assets/comics/dbz.jpg'
+import Battle from '../assets/comics/battle.jpg'
+import Heroes from '../assets/comics/heroes.jpg'
+import Xeno from '../assets/comics/xeno.jpg'
+import Zordon from '../assets/comics/zordon.jpg'
+
 const Comics = () => {
   return (
     <>
