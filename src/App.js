bf82e9155ec045dc72e5ccafc1e5ab8949ed3a32
Norbert Kovács <k.n.89@outlook.hu>
2023-01-22 16:45:49 +0100

    Deleted 404.html,index.html script,and basename in Router

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className='App'>
       //add basename
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
